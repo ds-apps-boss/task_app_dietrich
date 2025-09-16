3340583f8b21c73b077b771cadf170ed76c27aab
ds-apps-boss <ds-apps@sivoronov.de>
2025-09-16 16:04:01 +0200

    4531

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -4,6 +4,10 @@ class S4531 extends StatelessWidget {
   const S4531({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(children: [
+      Container(height: 100.0, width: 100.0, color: Colors.blueGrey,),
+      Container(height: 100.0, width: 100.0, color: Colors.blue,),
+      Container(height: 100.0, width: 100.0, color: Colors.cyan,),
+    ],);
   }
 }
