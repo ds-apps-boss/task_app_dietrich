710b81f95debca04aa1a9f175d3406a0622f5411
ds-apps-boss <ds-apps@sivoronov.de>
2025-09-17 12:38:34 +0200

    4536

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -1,9 +1,51 @@
 import 'package:flutter/material.dart';
 
+
+class Product {
+  final String name;
+  final double price;
+
+  const Product({
+    required this.name,
+    required this.price, 
+  });
+}
+
 class S4536 extends StatelessWidget {
   const S4536({super.key});
+
+  
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+
+    final List<Product> products =[
+      const Product(name: "BugFix", price: 123.45),
+      const Product(name: "Pi-Rechner", price: 3.14),
+      const Product(name: "Zero-Divider", price: 0.01),
+      const Product(name: "Null-Pointer Exception", price: 3222.51),
+      const Product(name: "RND-Generator", price: 94.50),
+    ];
+
+    
+    return ListView.builder(
+      shrinkWrap: true,
+      //physics: true ? const NeverScrollableScrollPhysics() :null,
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return ListTile(
+          leading: const Icon(Icons.shopping_cart),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(product.name),
+              Text("${product.price.toStringAsFixed(2)} €",),
+            ],
+          )
+          //title: Text(product.name),
+          //subtitle: Text(product.price.toStringAsFixed(2)),
+        );
+      }, 
+    );
   }
 }
