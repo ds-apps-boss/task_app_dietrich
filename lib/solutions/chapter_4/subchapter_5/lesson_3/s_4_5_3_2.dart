ca316a27a84e86f72926370c7e1fc5b1735de32d
ds-apps-boss <ds-apps@sivoronov.de>
2025-09-17 11:15:16 +0200

    4532

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_2.dart
@@ -4,6 +4,12 @@ class S4532 extends StatelessWidget {
   const S4532({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Column(
+      children: [
+        Text("App Akademie"),
+        Text("App Akademie", style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold, color: Colors.blue),),
+        Text("App Akademie", style: TextStyle(fontSize: 10.0, fontStyle: FontStyle.italic, color: Colors.green),),
+      ],
+    );
   }
 }
