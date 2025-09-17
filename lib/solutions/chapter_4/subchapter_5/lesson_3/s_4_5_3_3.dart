1e9a483974c53d9bec5c456c3463402c899baa1b
ds-apps-boss <ds-apps@sivoronov.de>
2025-09-17 11:26:59 +0200

    4533

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -4,6 +4,18 @@ class S4533 extends StatelessWidget {
   const S4533({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Container(
+      width: 150.0,
+      height: 150.0,
+      color: Colors.blue,
+      child: 
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("App"),
+            Text("Akademie"),
+        ],)
+    ,);
   }
 }
