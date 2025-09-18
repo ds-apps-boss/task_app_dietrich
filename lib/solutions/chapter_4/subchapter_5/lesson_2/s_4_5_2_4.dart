1aabafd6bf45084cab47a07bcc15d977e62115a7
ds-apps-boss <ds-apps@sivoronov.de>
2025-09-18 13:02:42 +0200

    4524

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,38 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
+  }
+}
+
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidget();
+}
+
+class _ShowHideNameWidget extends State<ShowHideNameWidget> {
+  final String myName = "Dietrich";
+  static const String btnNameOff = "Name anzeigen";
+  static const String btnNameOn  = "Name verstecken";
+  bool bState = false;
+
+  String get txtValue => bState ? myName : "";
+  String get btnTxt   => bState ? btnNameOn : btnNameOff;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column( 
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(txtValue),
+        ElevatedButton(
+          onPressed: () => setState(() => bState = !bState),
+          child: Text(btnTxt),
+        ),
+      ],
+    );
   }
 }
